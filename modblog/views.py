8e40427be0b59c1936f399e8b10321cc86d21492
Adam Feehely <adamfeehely@gmail.com>
2024-08-23 10:02:44 +0000

    add httpresponse to views.py for testing

--- modblog/views.py
+++ modblog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def my_modblog (response):
+    return HttpResponse("Hello, Modders!")
--- tewmodblog/urls.py
+++ tewmodblog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from modblog.views import my_modblog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('modblog/', my_modblog, name='modblog'),
 ]
