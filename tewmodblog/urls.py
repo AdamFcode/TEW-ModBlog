999cebcba929ae9f515eb6a38514496293c59b8c
Adam Feehely <adamfeehely@gmail.com>
2024-08-26 10:14:00 +0000

    add templates to modblog and remove httpresponse from views.py

--- tewmodblog/urls.py
+++ tewmodblog/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from modblog.views import my_modblog
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("modblog.urls"), name="modblog-urls"),
     path('admin/', admin.site.urls),
-    path('modblog/', my_modblog, name='modblog'),
 ]
