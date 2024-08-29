ecd304d5c210fcfad8c345f10dd89d1f05757467
Adam Feehely <adamfeehely@gmail.com>
2024-08-29 12:14:05 +0000

    edit postlist class and add notes

--- modblog/views.py
+++ modblog/views.py
@@ -4,6 +4,11 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    """
+    Queryset populates page with all published(status 1) posts
+    Template name dictates which template to load and render
+    Paginate by decides the number of posts per page
+    """
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
     paginate_by = 4
