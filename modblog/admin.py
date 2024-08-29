16f361d6dff885b1f7f237ffdb3eaa7fd03b10b3
Adam Feehely <adamfeehely@gmail.com>
2024-08-29 12:37:14 +0000

    edit postadmin class for better functionality and add notes to same

--- modblog/admin.py
+++ modblog/admin.py
@@ -4,10 +4,16 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    """
+    List Display dictates which fields to show on admin panel
+    Search fields speeds up the search by limiting the search paramaters
+    List filter allows superuser to filter by publication status
+    Prepopulated fields helps automatically populate the slug with title content
+    Summernote fields provides summernote functionality for greater editor features 
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
