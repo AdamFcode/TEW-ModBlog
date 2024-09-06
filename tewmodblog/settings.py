872bd6f07f3a559ae2fd9897c8ff3b3fc534cb05
Adam Feehely <adamfeehely@gmail.com>
2024-09-06 10:05:16 +0000

    edit base.html to load static

--- tewmodblog/settings.py
+++ tewmodblog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False       
+DEBUG = False         
 
 ALLOWED_HOSTS = ["8000-adamfcode-tewmodblog-h2tsun8ijja.ws.codeinstitute-ide.net", '.herokuapp.com']
 
