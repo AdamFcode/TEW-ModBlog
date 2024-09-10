0950fd5c6219cf40009c606aa3c95b83c5a754a2
Adam Feehely <adamfeehely@gmail.com>
2024-09-10 15:49:44 +0000

    edit link.html and related view for functionality

--- link/views.py
+++ link/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views import generic
 from .models import Link
 
 
-def link_list(request):
-    """
-    Renders the Link page
-    """
-    queryset = Link.objects.all()
-    link = get_object_or_404(queryset)
+def link(request):
+    link = Link.objects.all()
+    return render(request,'link/link.html', {"link": link} )
 
-    return render(
-        request,
-        "link/link.html",
-        {"link": link,},
-    )
+
