ce7045924d7421cad7cd3e9d22c1cea602b1f088
Adam Feehely <adamfeehely@gmail.com>
2024-09-26 13:44:25 +0000

    edit base.thml to change picture and its position

---
+++ staticfiles/js/comments.js
@@ -0,0 +1,32 @@
+import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
+window.bootstrap = bootstrap;  
+
+const editButtons = document.getElementsByClassName("edit-button");
+const commentText = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+const deleteModal = window.$('#deleteModal').modal();
+const deleteButtons = document.getElementsByClassName("delete-button");
+const closeModal = document.getElementById('closeModal');
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+
+
+for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    commentText.value = commentContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `edit_comment/${commentId}`);
+  });
+}
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("comment_id");
+    deleteConfirm.href = `delete_comment/${commentId}`;
+    deleteModal.show();
+  });
+}
