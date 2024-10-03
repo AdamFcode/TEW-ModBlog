f51eef60bc40aedbf577b90b0a5477d1230b58fa
Adam Feehely <adamfeehely@gmail.com>
2024-10-03 09:34:16 +0000

    add notation throughout

--- link/views.py
+++ link/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from .models import Link
 
 
+"""
+Requests and returns links and blurbs for the link section
+ """
 def link(request):
     link = Link.objects.all()
     return render(request,'link/link.html', {"link": link} )
--- static/js/comments.js
+++ static/js/comments.js
@@ -1,18 +1,20 @@
 import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
 window.bootstrap = bootstrap;  
 
+/*Declare constants for comment function*/
 const editButtons = document.getElementsByClassName("edit-button");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
+/*Declare constants for delete function*/
 const deleteModal = window.$('#deleteModal').modal();
 const deleteButtons = document.getElementsByClassName("delete-button");
 const closeModal = document.getElementById('closeModal');
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 
-
+/*Function to allow editing of comments*/
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
@@ -23,6 +25,7 @@ for (let button of editButtons) {
   });
 }
 
+/*Function for allowing deletion of comments*/
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
